5f1e18049cea95298b1c0efb38ebc4a08f9370b7
RonnyOtieno20 <ronnyotieno20@gmail.com>
2023-08-22 18:46:56 +0300

    updated main header file

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H /* check if main.h file is defined */
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdarg.h>
@@ -6,13 +6,29 @@
 #include <limits.h>
 #include <string.h>
 
+typedef int (*format_function)(va_list);
+
+/**
+ * struct format_mapper - struct to map format specifiers to
+ * their respective handler functions.
+ * @specifier: first member.
+ * @handler: second member.
+ */
+typedef struct format_mapper
+{
+	char specifier;
+	format_function handler;
+} format_mapper_t;
+
 int _printf(const char *format, ...);
-int _printf_int(int n);
 int _print_ch(va_list args);
 int _print_str(va_list args);
 int _print_pct(va_list args);
 int _print_int(va_list args);
 char *int_to_str(int n, char *b);
 char *strrev(char *s);
+void init_format_handlers(format_mapper_t *format_mappers);
+format_function fetch_handler
+(format_mapper_t *format_mappers, char specifiers);
 
 #endif /* MAIN_H */
