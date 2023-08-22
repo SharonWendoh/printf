93085450f7490844119063d98bc9a3bc5ff78b99
RonnyOtieno20 <ronnyotieno20@gmail.com>
2023-08-22 11:11:20 +0300

    header file

--- main.h
+++ main.h
@@ -3,11 +3,16 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
+int _printf_int(int n);
 int _print_ch(va_list args);
 int _print_str(va_list args);
 int _print_pct(va_list args);
-
+int _print_int(va_list args);
+char *int_to_str(int n, char *b);
+char *strrev(char *s);
 
 #endif /* MAIN_H */
