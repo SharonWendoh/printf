511b5775cf02ac73c5471473648e5f8e88e9d328
RonnyOtieno20 <ronnyotieno20@gmail.com>
2023-08-22 18:47:20 +0300

    format specifier functions

---
+++ format_specifiers_func.c
@@ -0,0 +1,97 @@
+#include "main.h"
+
+/**
+ * _print_ch - Prints a character.
+ * @args: The arguments list.
+ *
+ * Return: Number of characters printed.
+ */
+
+int _print_ch(va_list args)
+{
+
+	char c = va_arg(args, int);
+
+	write(1, &c, 1);
+	return (1);
+}
+
+/**
+ * _print_str - Prints a string.
+ * @args: The arguments list.
+ *
+ * Return: Number of characters printed.
+ */
+
+int _print_str(va_list args)
+{
+
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (!str)
+		str = "(null)";
+
+	for (; *str; str++, count++)
+	{
+		write(1, str, 1);
+	}
+
+	return (count);
+}
+/**
+ * _print_pct - Prints the percent symbol.
+ * @args: The argument list.
+ *
+ * Return: NUmber of characters printed.
+ */
+
+int _print_pct(va_list args)
+{
+	char c = '%';
+
+	(void)args; /* Indicate that args is intentionally not used. */
+
+	write(1, &c, 1);
+	return (1);
+}
+/**
+ * _printf_int - print integer as a string.
+ *@n: The integer to be printed.
+ *
+ * Return: Number of characters to be printed.
+ */
+
+int _printf_int(int n)
+{
+
+	char b[50];
+	char *s;
+
+	s = int_to_str(n, b);
+
+	write(1, s, strlen(s));
+	return (strlen(s));
+}
+
+/**
+ * _print_int - Prints the integer value.
+ * @args: The arguments list.
+ *
+ * Return: The number of characters printed.
+ */
+
+int _print_int(va_list args)
+{
+	int n = va_arg(args, int);
+	char b[21];
+	int i;
+
+	int_to_str(n, b);
+
+	for (i = 0; b[i]; i++)
+	{
+	write(1, &b[i], 1);
+	}
+	return (i);
+}
